eda9dc807163c21150f02319fb70b89dab92b980
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-02-18 11:08:43 +0700

    Jobsheet 2_Praktikum 1: Langkah 13_Route/articles/{id}

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,7 @@ Route::get('/posts/{post}/comments/{comment}',
 function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
