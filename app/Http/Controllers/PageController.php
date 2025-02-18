6cb44b43794c97b64556c0276e0550473116561e
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2025-02-18 14:13:05 +0700

    Jobsheet 2_Praktikum 2: Langkah 7_Single Action Controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,13 +6,5 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index() {
-        return 'Selamat Datang';
-    }
-    public function about() {
-        return 'Nama : Ayleen Ruhul Qisthy<br> NIM : 2341720012';
-    }
-    public function articles($id) {
-        return 'Halaman Artikel dengan ID '.$id;
-    }
+    //
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
@@ -23,13 +26,13 @@ Route::get('/', function () {
 Route::get('/hello', [WelcomeController::class,'hello']);
 
 // Route /
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 // Route /about
-Route::get('/about', [PageController::class, 'about']);
+Route::get('/about', [AboutController::class, 'about']);
 
 // Route /articles/{id}
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
 Route::get('/world', function () {
     return 'World';
